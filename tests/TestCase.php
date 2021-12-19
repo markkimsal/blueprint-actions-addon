475a26480a3e4a1dd449fde5dbeb54fb3b66b8c6
Mark Kimsal <metrofindings@gmail.com>
2021-12-19 14:39:32 -0500

    test: disable default blueprint generators in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,8 @@
 namespace Tests;
 
 use Blueprint\BlueprintServiceProvider;
+use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Markkimsal\BlueprintActionsAddon\Providers\BlueprintActionsAddonProvider;
 
 class TestCase extends \Orchestra\Testbench\TestCase
@@ -14,5 +16,17 @@ class TestCase extends \Orchestra\Testbench\TestCase
             BlueprintActionsAddonProvider::class,
             BlueprintServiceProvider::class,
         ];
+    }
+
+    /**
+     * Turn off default blueprint generators
+     * @param Application $app
+     * @return void
+     * @throws BindingResolutionException
+     */
+    protected function defineEnvironment($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('blueprint.generators', ['Markkimsal\BlueprintActionsAddon\LaravelActionsGenerator']);
     }
 }
